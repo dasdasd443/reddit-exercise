5d9440199d335126bee23f416034ac779694b634
dasdasd443 <cjvicro@gmail.com>
2021-05-20 19:54:16 +0800

    changes to index.js

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ xml.onreadystatechange = function() {
         let data = elem.data;
         let section = document.createElement("section");
         let title = document.createElement("h1");
+        console.log(data.url.search("v.redd.it"));
+        
         section.appendChild(title);
-        console.log(data.url.search("twitch"));
         if(data.url != ""){
-            let image = document.createElement("img");
-            image.setAttribute("src",data.url);
-            section.appendChild(image);
+            if(data.url.search("twitch") == 14){
+                let iframe = document.createElement("iframe");
+                iframe.setAttribute("src",data.url);
+                section.appendChild(iframe);
+            }
+            if(data.url.search("v.redd.it") == 32 || data.url.search("v.redd.it") == 8){
+                let iframe = document.createElement("iframe");
+                iframe.setAttribute("src",data.url);
+                section.appendChild(iframe);
+            }
+            if(data.url.search("jpg") == 32 || data.url.search("png") == 32){
+                console.log(data.url.search("jpg"));
+                let image = document.createElement("img");
+                image.setAttribute("src",data.url);
+                section.appendChild(image);
+            }
         }
         title.innerText = data.title;
         
